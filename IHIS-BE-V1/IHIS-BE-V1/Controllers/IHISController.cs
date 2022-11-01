a757d3a2d23cb304ebb3b2ba2aa1a8898091ec25
LAPTOP-V7RKI3PV\toanh <toan.hoang@team.enouvo.com>
2022-11-01 15:56:34 +0700

    build API

---
+++ IHIS-BE-V1/IHIS-BE-V1/Controllers/IHISController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IHIS_BE_V1.Controllers
+{
+    [ApiController]
+    [Route("IHIS")]
+    public class IHISController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
